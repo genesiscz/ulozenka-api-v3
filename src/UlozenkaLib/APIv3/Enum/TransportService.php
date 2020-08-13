cf1879659938a2c21ee9438d2b1cf4585785d3e3
Martin <admin@phpcode.eu>
2020-08-13 23:20:42 +0200

    updated transport services

--- src/UlozenkaLib/APIv3/Enum/TransportService.php
+++ src/UlozenkaLib/APIv3/Enum/TransportService.php
@@ -16,9 +16,18 @@ class TransportService
     const DPD_PICKUP = 5;
     const CPOST_BALIK_DO_RUKY = 6;
     const CPOST_BALIK_NA_POSTU = 7;
-    const INTIME_POSTOMAT_CZ = 8;
-    const INTIME_BALIKOMAT_SK = 9;
-	const ULOZENKA_PARTNER_PNS = 10;
+    //const INTIME_POSTOMAT_CZ = 8;
+    //const INTIME_BALIKOMAT_SK = 9;
+	//const ULOZENKA_PARTNER_PNS = 10;
 	const ULOZENKA_PARTNER = 11;
 	const INTIME_KURYR = 12;
+	const DHL = 16;
+	const ULOZENKA_B2C = 17;
+	const MALLBOX = 18;
+	const SKPOST_ECA  = 19;
+	const SKPOST_ECP = 20;
+	const SKPOST_ECB = 21;
+	const DEPO = 22;
+	const DEPO_SK_SK = 24;
+	
 }
